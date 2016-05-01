f5e850395895a0f42362954fb313e8b952b8db13
Andre Caron <andre.l.caron@gmail.com>
2016-04-30 23:33:23 -0400

    Polishes project setup.

--- README.md
+++
@@ -1,1 +0,0 @@
-"# gitmesh-deploy" 
--- setup.py
+++ setup.py
@@ -13,15 +13,16 @@ def readfile(path):
         return content
 
 version = readfile('gitmesh_deploy/version.txt').strip()
+readme = readfile('README.rst').strip()
 
 setup(
-    name='gitmesh_deploy',
+    name='gitmesh-deploy',
     version=version,
     url='https://github.com/smartmob-project/pushy',
     maintainer='Nicolas Mivielle',
-    maintainer_email='sonic1200@gmail.com',
+    maintainer_email='nm@smartmob.org',
     description='Something that will push your code',
-    long_description='no',
+    long_description=readme,
     packages=find_packages(),
     package_data={
         'gitmesh_deploy': [
@@ -29,7 +30,9 @@ setup(
         ],
     },
     entry_points={
-        'gitmesh.post_receive': ["gitmesh_deploy = gitmesh_deploy:post_receive"],
+        'gitmesh.post_receive': [
+            'gitmesh_deploy = gitmesh_deploy:post_receive'
+        ],
         'gitmesh.post_update': [],
     },
 )
